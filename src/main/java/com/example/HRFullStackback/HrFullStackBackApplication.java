c90b5c4a9743e865df03c030789209d812f14d35
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-03-07 11:13:36 +0100

    initial backend commit

---
+++ src/main/java/com/example/HRFullStackback/HrFullStackBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.HRFullStackback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrFullStackBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrFullStackBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HRFullStackback/HrFullStackBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HRFullStackback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrFullStackBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
