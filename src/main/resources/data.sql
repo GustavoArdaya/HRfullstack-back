84916fb2dcc27ce609bbc65d38ce34e4fbbf13f0
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-03-20 10:10:50 +0100

    add data.sql and migrate former in memoryRepository to SQL h2 repository

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+
+INSERT
+INTO
+  employees
+  (name, last_name, photo, position, phone, email, location, dni, salary, joining_date, birth_date)
+VALUES
+  ('Perico','Lospa Lotes', './photos/default.png', 'Currante','600123456','perico@lospa.com','Barcelona','12345678Z',1234.56,'2020-04-27','1987-07-05'),
+  ('Mihaela','Gheorghiu','./photos/MihaelaGheorghiu.jpg', 'Currante','600123456','perico@lospa.com','Barcelona','12345678Z',1234.56,'2020-04-27','1987-07-05'),
+  ('Gustavo','Ardaya', './photos/GustavoArdaya.jpg', 'Currante','600123456','perico@lospa.com','Barcelona','12345678Z',1234.56,'2020-04-27','1987-07-05'),
+  ('Daniel','Gallego' ,'./photos/DanielGallego.png', 'Currante','600123456','perico@lospa.com','Barcelona','12345678Z',1234.56,'2020-04-27', '1987-07-05'),
+  ('Jaume','Vidal' ,'./photos/JaumeVidal.png', 'Currante','600123456','perico@lospa.com','Barcelona','12345678Z',1234.56, '2020-04-27','1989-01-01');
