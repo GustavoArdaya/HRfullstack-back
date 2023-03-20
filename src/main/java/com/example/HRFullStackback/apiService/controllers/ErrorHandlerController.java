3f121433249ba42f1191bee2152fa952e0331a48
jaume vidal <jaumevibu@gmail.com>
2023-03-20 14:56:17 +0100

    add ErrorHandlerController, Department entity

---
+++ src/main/java/com/example/HRFullStackback/apiService/controllers/ErrorHandlerController.java
@@ -0,0 +1,9 @@
+package com.example.HRFullStackback.apiService.controllers;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ErrorHandlerController extends ResponseEntityExceptionHandler {
+
+}
---
+++ src/main/java/com/example/HRFullStackback/domain/models/Department.java
@@ -0,0 +1,15 @@
+package com.example.HRFullStackback.domain.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "departments")
+@Data
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+}
