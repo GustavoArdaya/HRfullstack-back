b3e22b6ab25188107ce7255406ad284c930adab9
Daniel-gallegO <danielgallegolopez@outlook.com>
2023-03-20 11:10:14 +0100

    added data to SQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,4 +8,5 @@ VALUES
   ('Mihaela','Gheorghiu','./photos/MihaelaGheorghiu.jpg', 'Currante','600123456','perico@lospa.com','Barcelona','12345678Z',1234.56,'2020-04-27','1987-07-05'),
   ('Gustavo','Ardaya', './photos/GustavoArdaya.jpg', 'Currante','600123456','perico@lospa.com','Barcelona','12345678Z',1234.56,'2020-04-27','1987-07-05'),
   ('Daniel','Gallego' ,'./photos/DanielGallego.png', 'Currante','600123456','perico@lospa.com','Barcelona','12345678Z',1234.56,'2020-04-27', '1987-07-05'),
-  ('Jaume','Vidal' ,'./photos/JaumeVidal.png', 'Currante','600123456','perico@lospa.com','Barcelona','12345678Z',1234.56, '2020-04-27','1989-01-01');
+  ('Jaume','Vidal' ,'./photos/JaumeVidal.png', 'Currante','600123456','perico@lospa.com','Barcelona','12345678Z',1234.56, '2020-04-27','1989-01-01'),
+  ('Ana Ludovika','Nieto Navarro','https://th.bing.com/th/id/R.3e6639b601e07e09e6b5c9382ea7f8ce?rik=7ca%2bYuJThoOfmw&riu=http%3a%2f%2fwww.accelpt.net%2fimages%2fslider%2fperson+(1).png&ehk=CVN2VY8L%2f6IBOJrEf%2b%2bG8umpds4Npn6PIPoGXVq2EA8%3d&risl=&pid=ImgRaw&r=0','Directora de ventas','668943169','ananieto@gmail.com','Barcelona','52834512Q',2000.00,'2014-07-12','1989-05-20');
